c20bfa3960c79e11e2558bb694566c0b68b37da8
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2025-03-26 04:17:22 +0500

    fixed

--- src/pages/dashboard/student/individual/[id]/[test]/index.js
+++ src/pages/dashboard/student/individual/[id]/[test]/index.js
@@ -7,6 +7,10 @@ import Image from "next/image";
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 import InfoCircleIcon from "@/components/icons/info-circle";
 import WarningModal from "@/components/modal/warning-modal";
+import { AnimatePresence, motion } from "framer-motion";
+import SimpleModal from "@/components/modal/simple-modal";
+import { dropRight } from "lodash";
+import Link from "next/link";
 
 const mathSymbols = {
   "√x": "\\sqrt{x}",
@@ -75,6 +79,8 @@ const Index = () => {
   const [selectedQuestion, setSelectedQuestion] = useState(null);
   const router = useRouter();
   const [showWarning, setShowWarning] = useState(false);
+  const [showCalculator, setShowCalculator] = useState(false);
+  const [showMistake, setShowMistake] = useState(false);
 
   const handleShowWarning = () => {
     setShowWarning(true);
@@ -87,16 +93,26 @@ const Index = () => {
     setInput((prev) => prev + (mathSymbols[btn] || btn));
   };
 
-  const clearInput = () => setInput("");
   return (
     <div className="font-sf">
-      <div className="flex justify-between pl-[24px] pr-[16px] py-[14px] border-b border-b-[#F2F2F7]">
+      <div className="flex justify-between pl-[24px] pr-[16px] py-[14px] border-b border-b-[#F2F2F7] items-center">
         <div className="flex items-center gap-x-[12px]">
           <h1 className="text-[22px] font-semibold">Теория</h1>
           <div className="w-[1px] h-[26px] bg-[#E9E9E9]"></div>
           <p className="text-[17px] text-[#525252]">
             Задача (Elixir.Task_5_3_5(x1))
           </p>
+        </div>
+
+        <div>
+          <button onClick={() => router.back()} className="float-right rounded">
+            <Image
+              src={"/icons/close.svg"}
+              alt="circle"
+              width={24}
+              height={24}
+            />
+          </button>
         </div>
       </div>
 
@@ -167,9 +183,13 @@ const Index = () => {
                     <MathJax>{`\\(${input}\\)`}</MathJax>
                   </div>
 
-                  <div className="flex justify-between">
-                    <div className="flex  items-center relative">
-                      <Button px="px-[16px]" py="py-[11px]">
+                  <div className="flex justify-between items-center">
+                    <div className="flex relative">
+                      <Button
+                        onclick={() => setShowMistake(true)}
+                        px="px-[16px]"
+                        py="py-[11px]"
+                      >
                         Проверить
                       </Button>
 
@@ -183,7 +203,7 @@ const Index = () => {
                         Показать решение
                       </Button>
 
-                      <div className="p-[6px] mr-[20px] cursor-pointer ">
+                      <div className="p-[6px] mr-[20px] cursor-pointer flex items-center ">
                         <button onClick={handleShowWarning}>
                           <InfoCircleIcon
                             color={!showWarning ? "#4D555DFF" : "#F97316FF"}
@@ -202,42 +222,109 @@ const Index = () => {
                         )}
                       </div>
 
-                      <div className="p-[6px] mr-[20px] cursor-pointer">
-                        <Image
-                          src="/icons/calculator.svg"
-                          alt="info"
-                          width={28}
-                          height={28}
-                        />
+                      <div className="p-[6px] mr-[20px] cursor-pointer flex items-center">
+                        <button
+                          onClick={() => setShowCalculator(!showCalculator)}
+                        >
+                          <Image
+                            src="/icons/calculator.svg"
+                            alt="info"
+                            width={28}
+                            height={28}
+                          />
+                        </button>
                       </div>
                     </div>
 
                     <div className="p-[4px]">3 попытки</div>
                   </div>
 
-                  <div className="bg-[#E9E9E9] w-full h-[1px] p-0"></div>
+                  {!showCalculator && (
+                    <AnimatePresence>
+                      <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 50 }}
+                        transition={{ duration: 0.3 }}
+                        className=""
+                      >
+                        <div className="bg-[#E9E9E9] w-full h-[1px] p-0"></div>
 
-                  <div className="space-y-[32px] p-4">
-                    <div className="grid grid-cols-10 gap-2">
-                      {buttons.map((btn, i) => (
-                        <button
-                          key={i}
-                          className={`p-3 w-[60px] h-[50px] text-xl font-normal rounded shadow ${
-                            ["-", "×", "÷", "+"].includes(btn)
-                              ? "text-white bg-orange-500 hover:bg-orange-600"
-                              : "text-[#59626B] bg-[#F5F6F8] hover:bg-gray-300"
-                          }`}
-                          onClick={() => handleClick(btn)}
-                        >
-                          <MathJax>{`\\(${
-                            mathSymbols[btn] || btn
-                          }\\)`}</MathJax>
-                        </button>
-                      ))}
-                    </div>
-                  </div>
+                        <div className="space-y-[32px] p-4">
+                          <div className="grid grid-cols-10 gap-2">
+                            {buttons.map((btn, i) => (
+                              <button
+                                key={i}
+                                className={`p-3 w-[60px] h-[50px] text-xl font-normal rounded shadow ${
+                                  ["-", "×", "÷", "+"].includes(btn)
+                                    ? "text-white bg-orange-500 hover:bg-orange-600"
+                                    : "text-[#59626B] bg-[#F5F6F8] hover:bg-gray-300"
+                                }`}
+                                onClick={() => handleClick(btn)}
+                              >
+                                <MathJax>{`\\(${
+                                  mathSymbols[btn] || btn
+                                }\\)`}</MathJax>
+                              </button>
+                            ))}
+                          </div>
+                        </div>
+                      </motion.div>
+                    </AnimatePresence>
+                  )}
                 </div>
               </MathJaxContext>
+
+              {showMistake && (
+                <SimpleModal>
+                  <div>
+                    <div className="flex justify-between px-[16px] py-[18px]">
+                      <h3 className="text-[19px] font-semibold">Мои ошибки</h3>
+                      <button
+                        onClick={() => setShowMistake(false)}
+                        className="rounded"
+                      >
+                        <Image
+                          src={"/icons/close.svg"}
+                          alt="circle"
+                          width={24}
+                          height={24}
+                        />
+                      </button>
+                    </div>
+
+                    <div className="bg-[#E9E9E9] w-full h-[1px] p-0"></div>
+
+                    <div className="py-[16px] px-[24px]">
+                      <ul className="space-y-2">
+                        {dropRight(questions, 10).map((question, index) => (
+                          <li
+                            key={index}
+                            className={`p-3  rounded-md flex items-center gap-x-[12px] cursor-pointer `}
+                            onClick={() => setSelectedQuestion(question)}
+                          >
+                            <div
+                              className={`min-w-10 min-h-10 flex items-center justify-center border-2 border-[#FF3B30]  bg-[#FFEBEA] rounded-full text-black font-bold`}
+                            >
+                              {index + 1}
+                            </div>
+                            <p>{question}</p>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+
+                    <div className="bg-[#E9E9E9] w-full h-[1px] p-0"></div>
+
+                    <Link
+                      href={"/dashboard/student/subjects/1"}
+                      className="flex items-center justify-center py-[16px]"
+                    >
+                      <Button>Рекомендация</Button>
+                    </Link>
+                  </div>
+                </SimpleModal>
+              )}
             </div>
           ) : (
             <div className="p-4 text-gray-500 italic">Savolni tanlang...</div>
